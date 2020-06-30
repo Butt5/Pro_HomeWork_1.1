be43e58b9b00aa1de8a53f0e4e9c2fabee631b3c
Vitaliy Dementev <butt_899@mail.ru>
2020-06-30 16:02:15 +0300

    Upgrade main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from application import salary
 from db import people
+import datetime
 
 def budget():
     budget_salary = (salary.calculate_salary() * people.get_employees())
     return budget_salary
 
 if __name__ == '__main__':
-    print(f'Общий зарплатный бюджет всех сотрудников составляет: ', budget())
+    print('Общий зарплатный бюджет всех сотрудников составляет: ', budget())
+    print('Текущая дата :', datetime.datetime.today())
